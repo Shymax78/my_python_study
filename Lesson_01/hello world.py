37db35406b96049ec1ef9cb8d2aa405d427359a6
Shymaks78 <maks.shymanko@gmail.com>
2024-11-13 17:32:08 +0200

    Final_01

--- Lesson_01/hello world.py
+++ Lesson_01/hello world.py
@@ -20,11 +20,12 @@ none = ()
 print (type(none))
 
 num_str = 125
-string = str("num_str")
+string = str(num_str)
 print (string)
 print (type(string))
 
 string = "Hi, my name is Python!"
+print (string)
 string = string.replace ("y", "0",)
 print (string)
 string = string.replace ("i", "1")
@@ -35,7 +36,33 @@ split_test = split_test.split()
 print (split_test)
 
 
-string = str (split_test)
-string = string.join(split_test)
-split_test = string
-print (split_test)
+
+string_join = " ".join(split_test)
+print (string_join)
+
+print(len(string_join))
+
+
+
+list_append = [1, 2, 3]
+print(len(list_append))
+list_append.append (4)
+list_append.append (5)
+print (list_append)
+
+list_extend = [4, 5, 6]
+print(list_extend)
+list_extend.extend([7, 8, 9])
+print(list_extend)
+print(list_extend.index(6))
+print(len(list_append))
+
+
+dict_test = {'car': 'Toyota', 'price': 4900, 'where': 'EU'}
+
+print(dict_test["car"], dict_test["where"])
+
+print(dict_test.keys())
+print(dict_test.values())
+print(dict_test.items())
+
